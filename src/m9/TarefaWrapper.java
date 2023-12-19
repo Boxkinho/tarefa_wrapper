b334f9d824bb2caee52d8c4be3b8b5b680e4315a
boxkinho <jbmmf@yahoo.com.br>
2023-12-18 21:27:22 -0300

    Correção M9

--- src/m9/TarefaWrapper.java
+++ src/m9/TarefaWrapper.java
@@ -7,14 +7,14 @@ public class TarefaWrapper {
 		//casting explicito
 		long numero1 = 1000000000000000000l;
 		System.out.println(numero1);
-		int numero2 = (int) numero1;
-		System.out.println(numero1);
+		Long numero2 = (Long) numero1;
+		System.out.println(numero2);
 				
 		//casting implicito
 		int nota1 = 1000000000;
 		System.out.println(nota1);
-		long nota2 = nota1;
-		System.out.println(nota1);
+		Integer nota2 = nota1;
+		System.out.println(nota2);
 
 	}
 
